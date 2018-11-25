7d52c425f567d43f871b513e65045e95b3f8dc3c
Gilles Kagarama <kagaramag@gmail.com>
2018-11-25 21:39:17 +0200

    listen to port conflict

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ import app from './app';
 const port = process.env.PORT || 3100;
 const server = http.createServer(app);
 
+// Handling exceptions
+process.on('uncaughtException', (error) => {
+    if (error.code === 'EADDRINUSE') {
+     console.log(`Conflict with post ${port}`);
+    }
+  })
 
 server.listen(port, () => {
     console.log(`Server starts on port: ${port}`)
