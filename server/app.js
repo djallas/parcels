7c9911166a8759f5c3915fdc6b6d1dde0ac9147c
Gilles Kagarama <kagaramag@gmail.com>
2018-11-26 20:36:25 +0200

    migration

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
 // import routes handlers
-import ErrorController from './controllers/ErrorController';
 
 const app = express();
 
@@ -23,15 +22,14 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+import pool from './db/config';
 // queries
 app.get('/cp', (req,res) =>{
-    pool.query("CREATE TABLE memb(id SERIAL PRIMARY KEY, firstname VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL)", (err, res) => {
+    pool.query("CREATE TABLE membs(id SERIAL PRIMARY KEY, firstname VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL)", (err, res) => {
         console.log(res)
         pool.end();
     });
 });
 
-app.use(ErrorController.NotFound);
-app.use(ErrorController.InternalServerError);
 
 module.exports = app;
